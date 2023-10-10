45c4d62fb2267fcd0fe4e3bc15a5c8b0cb92704d
AHIH31 <ahmed.haythamih@gmail.com>
2023-10-10 22:00:50 +0300

    Doctor Register done

--- client/front-end/src/pages/Doctor/Register.tsx
+++ client/front-end/src/pages/Doctor/Register.tsx
@@ -1,9 +1,10 @@
 import React, {useState,ChangeEvent} from 'react';
+import axios from "axios";
 import { IonIcon } from '@ionic/react';
 
 const steps = [
-    {id: 1, title:'Personal Info', fields: ['Name','Date of Birth', 'Email']},
-    {id: 2, title:'Logging Info', fields: ['Username', 'Password', 'Confirm Password']},
+    {id: 1, title:'Personal Info', fields: ['Name','Date of Birth', 'Speciality']},
+    {id: 2, title:'Logging Info', fields: ['Username', 'Password', 'Email']},
     {id: 3, title:'Professional Info', fields: ['Hourly Rate', 'Affiliation', 'Educational Background']},
 ];
 
@@ -17,7 +18,39 @@ function Register(){
     const [HourlyRate, setHourlyRate] = useState<Number>();
     const [Dob, setDob] = useState<Date>();
     const [Affiliation, setAffiliation] = useState<string>("");
-    const [Education, setEducation] = useState<string>("");
+    const [EducationalBackground, setEducation] = useState<string>("");
+    const [Specialty, setSpecialty] = useState<string>("");
+
+
+    const api = axios.create({
+      baseURL: "http://localhost:8000/",
+  });
+
+    const handleSignUp = async () => {
+      try {
+        const data = {
+          Name,
+          Email,
+          Password,
+          Username,
+          Dob,
+          HourlyRate,
+          Affiliation,
+          EducationalBackground,
+          Specialty
+  
+        };
+        
+       
+        const response = await api.post(`/doctor/register`, data);
+        console.log("Response:", response.data);
+        setModalActive(true);
+        setTimeout(closeModal,1500)
+       
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
 
 
     const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -32,6 +65,9 @@ function Register(){
       };
       const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
         setEmail(event.target.value);
+      };
+      const handleSpecialtyChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSpecialty(event.target.value);
       };
       const handleHourRateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = event.target.value;
@@ -73,9 +109,6 @@ function Register(){
       }
     };
   
-    const handleDone = () => {
-      setModalActive(true);
-    };
   
     const closeModal = () => {
       setModalActive(false);
@@ -85,7 +118,7 @@ function Register(){
     return (
     <div className="wrapper">
         <div className="header">
-              <h1>Apply to become a doctor</h1>
+              <h1 className='h1'>Apply to become a doctor</h1>
       </div>
 
 
@@ -93,19 +126,37 @@ function Register(){
         <div className="header">
 
           <ul>
-            {steps.map((step) => (
-              <li key={step.id} className={`form_${step.id}_progessbar ${activeForm >= step.id ? 'active' : ''}`}>
+              <li key={1} className={`form_${1}_progessbar ${activeForm >= 1 ? 'active' : ''}`}>
                 <div>
-                  <p>{step.id}</p>
+                  <p>{1}</p>
                 </div>
               </li>
-            ))}
+          
+          </ul>
+          
+          <ul>
+              <li key={2} className={`form_${2}_progessbar ${activeForm >= 2 ? 'active' : ''}`}>
+                <div>
+                  <p>{2}</p>
+                </div>
+              </li>
+          
+          </ul>
+          
+          <ul>
+              <li key={3} className={`form_${3}_progessbar ${activeForm >= 3 ? 'active' : ''}`}>
+                <div>
+                  <p>{3}</p>
+                </div>
+              </li>
+          
           </ul>
         </div>
+
         <div className="form_wrap">
-          {steps.map((step) => (
-            <div key={step.id} className={`form_${step.id} data_info`} style={{ display: activeForm === step.id ? 'block' : 'none' }}>
-              <h2>{step.title}</h2>
+          
+            <div key={1} className={`form_1 data_info`} style={{ display: activeForm === 1 ? 'block' : 'none' }}>
+              <h2>{"Personal Info"}</h2>
               <form>
                 <div className="form_container">
                 
@@ -125,6 +176,48 @@ function Register(){
         className="input"
         value = {Dob !== undefined ? Dob.toISOString().split('T')[0] : ''}
         onChange={handleDobChange}
+        type="date  "/>
+                    </div>
+
+
+                    <div key= "3 " className="input_wrap">
+      <label htmlFor="Name">Specialty</label>
+      <input
+        className="input"
+        value = {Specialty}
+        onChange={handleSpecialtyChange}
+        type="text"/>
+                    </div>
+                    
+                </div>
+              </form>
+            </div>
+        </div>
+
+
+<div className="form_wrap">
+          
+            <div key={2} className={`form_2 data_info`} style={{ display: activeForm === 2 ? 'block' : 'none' }}>
+              <h2>{"Logging Info"}</h2>
+              <form>
+                <div className="form_container">
+                
+    <div key= "1" className="input_wrap">
+      <label htmlFor="Username">Username</label>
+      <input
+        className="input"
+        value = {Username}
+        onChange={handleUsernameChange}
+        type="text"/>
+                    </div>
+
+
+                    <div key= "2" className="input_wrap">
+      <label htmlFor="Name">Password</label>
+      <input
+        className="input"
+        value = {Password}
+        onChange={handlePasswordChange}
         type="text"/>
                     </div>
 
@@ -141,8 +234,51 @@ function Register(){
                 </div>
               </form>
             </div>
-          ))}
         </div>
+
+
+        <div className="form_wrap">
+          
+          <div key={3} className={`form_3 data_info`} style={{ display: activeForm === 3 ? 'block' : 'none' }}>
+            <h2>{"Professional Info"}</h2>
+            <form>
+              <div className="form_container">
+              
+  <div key= "1" className="input_wrap">
+    <label htmlFor="HourRate">Hourly Rate</label>
+    <input
+      className="input"
+      value = {HourlyRate !== undefined ? HourlyRate.toString() : ''}
+      onChange={handleHourRateChange}
+      type="text"/>
+                  </div>
+
+
+                  <div key= "2" className="input_wrap">
+    <label htmlFor="Afilliation">Afilliation</label>
+    <input
+      className="input"
+      value = {Affiliation}
+      onChange={handleAffiliationChange}
+      type="text"/>
+                  </div>
+
+
+                  <div key= "3 " className="input_wrap">
+    <label htmlFor="Education">Educational Background</label>
+    <input
+      className="input"
+      value = {EducationalBackground}
+      onChange={handleEducationchange}
+      type="text"/>
+                  </div>
+                  
+              </div>
+            </form>
+          </div>
+      </div>
+
+
         <div className="btns_wrap">
           <div className={`common_btns form_${activeForm}_btns`}>
             {activeForm !== 1 && (
@@ -156,15 +292,15 @@ function Register(){
                 Next <span className="icon"><IonIcon name="arrow-forward-sharp"></IonIcon></span>
               </button>
             ) : (
-              <button type="button" className="btn_done" onClick={handleDone}>
-                Done
+              <button type="button" className="btn_done" onClick={handleSignUp}>
+                Submit
                 
               </button>
             )}
           </div>
         </div>
         <div className={`modal_wrapper ${modalActive ? 'active' : ''}`}>
-          <div className="shadow" onClick={closeModal}></div>
+          <div className="shadow"  ></div>
           <div className="success_wrap">
             <span className="modal_icon"><IonIcon name="checkmark-sharp"></IonIcon></span>
             <p>You have successfully completed the process.</p>
